823d5449102fbe7a42310cf537041078bce13874
BreugelmansD <breugelmansdimi@gmail.com>
2026-09-06 15:20:57 +0200

    Update firebaseConfig.js

--- src/firebaseConfig.js
+++ src/firebaseConfig.js
@@ -5,12 +5,13 @@
 // Zolang apiKey op "VUL_IN" staat, toont de app een setup-scherm in plaats van
 // te proberen in te loggen (zie src/main.js).
 export const firebaseConfig = {
-  apiKey: 'VUL_IN',
-  authDomain: 'VUL_IN.firebaseapp.com',
-  projectId: 'VUL_IN',
-  storageBucket: 'VUL_IN.appspot.com',
-  messagingSenderId: 'VUL_IN',
-  appId: 'VUL_IN',
+  apiKey: "AIzaSyDBtnfDLc-NH20VuDJnx22-VXBZFFrVDyY",
+    authDomain: "av-double-check.firebaseapp.com",
+    projectId: "av-double-check",
+    storageBucket: "av-double-check.firebasestorage.app",
+    messagingSenderId: "152069939641",
+    appId: "1:152069939641:web:028e152c06499a4b239668",
+  appId: '1:152069939641:web:028e152c06499a4b239668',
 };
 
 // Enkel Google-accounts met dit e-maildomein krijgen toegang tot de app.
